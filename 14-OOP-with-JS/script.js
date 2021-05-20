7ffa9806e31dce67fc167f415a1b047ac82121e2
Howie Hao Wang <howiewang2020@gmail.com>
2021-05-20 16:47:32 -0400

    update script.js with what is OOP

--- 14-OOP-with-JS/script.js
+++ 14-OOP-with-JS/script.js
@@ -1,5 +1,5 @@
-'use strict';
-// NOTE Index
+"use strict";
+// INDEX
 // 1. What is Object-Oriented Programming?
 // 2. OOP in JavaScript
 // 3. Constructor Functions and the new Operator
@@ -7,19 +7,13 @@
 // 5. Prototypal Inheritance and The Prototype Chain
 // 6. Prototypal Inheritance on Built-In Objects
 // Coding Challenge #1
-///////////////////////////////////////////////////
-
 // 7. ES6 Classes
 // 8. Setters and Getters
 // 9. Static Methods
 // 10. Object.create
 // Coding Challenge #2
-///////////////////////////////////////////////////
-
 // 11. Inheritance Between "Classes": Constructor Functions
 // Coding Challenge #3
-///////////////////////////////////////////////////
-
 // 12. Inheritance Between "Classes": ES6 Classes
 // 13. Inheritance Between "Classes": Object.create
 // 14. Another Class Example
@@ -29,8 +23,15 @@
 // 18. ES6 Classes Summary
 // Coding Challenge #4
 ///////////////////////////////////////////////////
+// NOTE 1. What is Object-Oriented Programming?
+// 1.1 OOP is a programming paradigm based on the concept of objects
+// 1.2 Four principles
+// (1) Abstraction: ignoring or hiding details that DON'T MATTER
+// (2) Encapsulation: keeping properties and methods PRIVATE inside the class
+// (3) Inheritance: making all properties and methods of a certain class AVAILABLE TO A CHILD CLASS
+// (4) Polymorphism: a child class can OVERWRITE a method it inherited from a parent class
+///////////////////////////////////////////////////
 
-// 1. What is Object-Oriented Programming?
 // 2. OOP in JavaScript
 // 3. Constructor Functions and the new Operator
 // 4. Prototypes
