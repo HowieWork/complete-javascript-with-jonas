01905cf4de7dfd77ca06d8920aa5f31b873fb963
Howie Hao Wang <howiewang2020@gmail.com>
2021-05-20 15:48:14 -0400

    initial commit

---
+++ 14-OOP-with-JS/script.js
@@ -0,0 +1,61 @@
+'use strict';
+// NOTE Index
+// 1. What is Object-Oriented Programming?
+// 2. OOP in JavaScript
+// 3. Constructor Functions and the new Operator
+// 4. Prototypes
+// 5. Prototypal Inheritance and The Prototype Chain
+// 6. Prototypal Inheritance on Built-In Objects
+// Coding Challenge #1
+///////////////////////////////////////////////////
+
+// 7. ES6 Classes
+// 8. Setters and Getters
+// 9. Static Methods
+// 10. Object.create
+// Coding Challenge #2
+///////////////////////////////////////////////////
+
+// 11. Inheritance Between "Classes": Constructor Functions
+// Coding Challenge #3
+///////////////////////////////////////////////////
+
+// 12. Inheritance Between "Classes": ES6 Classes
+// 13. Inheritance Between "Classes": Object.create
+// 14. Another Class Example
+// 15. Encapsulation: Protected Properties and Methods
+// 16. Encapsulation: Private Class Field and Methods
+// 17. Chaining Methods
+// 18. ES6 Classes Summary
+// Coding Challenge #4
+///////////////////////////////////////////////////
+
+// 1. What is Object-Oriented Programming?
+// 2. OOP in JavaScript
+// 3. Constructor Functions and the new Operator
+// 4. Prototypes
+// 5. Prototypal Inheritance and The Prototype Chain
+// 6. Prototypal Inheritance on Built-In Objects
+// Coding Challenge #1
+///////////////////////////////////////////////////
+
+// 7. ES6 Classes
+// 8. Setters and Getters
+// 9. Static Methods
+// 10. Object.create
+// Coding Challenge #2
+///////////////////////////////////////////////////
+
+// 11. Inheritance Between "Classes": Constructor Functions
+// Coding Challenge #3
+///////////////////////////////////////////////////
+
+// 12. Inheritance Between "Classes": ES6 Classes
+// 13. Inheritance Between "Classes": Object.create
+// 14. Another Class Example
+// 15. Encapsulation: Protected Properties and Methods
+// 16. Encapsulation: Private Class Field and Methods
+// 17. Chaining Methods
+// 18. ES6 Classes Summary
+// Coding Challenge #4
+///////////////////////////////////////////////////
---
+++ README.md
@@ -0,0 +1,14 @@
+# Complete JavaScript with Jonas
+
+## Brief
+
+This repo is created as a record of my JavaScript learning from Jonas Schmedtmann.
+
+It includes study notes, code challenges/exercises and projects for my own study purpose.
+
+Course link:
+[The Complete JavaScript Course 2021: From Zero to Expert!](https://www.udemy.com/course/the-complete-javascript-course/?referralCode=87FE8B1039A68106DEE5)
+
+## Projects
+
+(updating)
