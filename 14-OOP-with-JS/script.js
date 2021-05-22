5988608c365e345967218e20591fedcf04a5aaa1
Howie Hao Wang <howiewang2020@gmail.com>
2021-05-22 06:11:39 -0400

    add inheritance between classes using ES6 class in sec14 oop

--- 14-OOP-with-JS/script.js
+++ 14-OOP-with-JS/script.js
@@ -412,6 +412,33 @@ const carTesla = new EV('Tesla', 120, 23);
 ///////////////////////////////////////////////////
 
 // NOTE 12. Inheritance Between "Classes": ES6 Classes
+// EXTENDS SUPER
+// if no extra, 'class StudentCl extends PersonCl' will be enough
+class StudentCl extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    // Always needs to happen first!
+    super(fullName, birthYear);
+    this.course = course;
+  }
+
+  introduce = function () {
+    console.log(`My name is ${this.fullName} and I study ${this.course}.`);
+  };
+
+  calcAge() {
+    console.log(
+      `I'm ${
+        2037 - this.birthYear
+      } years old, but as a student I feel more like ${
+        2037 - this.birthYear + 10
+      }`
+    );
+  }
+}
+
+const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
+martha.introduce();
+martha.calcAge();
 ///////////////////////////////////////////////////
 
 // NOTE 13. Inheritance Between "Classes": Object.create
