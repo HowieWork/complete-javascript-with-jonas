a1d2eedd647a4d4e6d31e16955efed7c8a965826
Howie Hao Wang <howiewang2020@gmail.com>
2021-06-02 16:49:14 -0400

    Update script.js for section 7

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,1 +1,20 @@
 'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 23;
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  // IMPORTANT input value will be a STRING
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number!';
+  }
+});
