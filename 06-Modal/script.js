a5037dc3f8f7b66a1cb6183bb3aa9deaaa236def
Howie Hao Wang <howiewang2020@gmail.com>
2021-06-04 09:54:38 -0400

    Update sec.7 Basic DOM: complete Modal Window project

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const showModalBtns = document.querySelectorAll('.show-modal');
+const closeModalBtn = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+const showModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+showModalBtns.forEach(showBtn => {
+  showBtn.addEventListener('click', showModal);
+});
+
+// Close modal by clicking close button
+closeModalBtn.addEventListener('click', closeModal);
+
+// Close modal by clicking overlay area
+overlay.addEventListener('click', closeModal);
+
+// Close modal by pressing ESC
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
