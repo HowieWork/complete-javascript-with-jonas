d85529988bbc298832cb2ab47c76508f9cdcf122
Howie Hao Wang <howiewang2020@gmail.com>
2021-06-07 11:42:37 -0400

    Update script.js in sec8: an high-level overview

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -15,7 +15,43 @@
 ///////////////////////////////////////////////////
 
 // NOTE 1. An High-Level Overview of JavaScript
+// 1.1 High-level
+// (1) Low-level: Developer has to manage resources manually
+// (2) High-level: Developer does NOT have to worry, everything happens automatically
 
+// 1.2 Garbage-collected
+// Cleaning the memory so we don't have to
+
+// 1.3 Interpreted or just-in-time compiled
+// (1) Abstraction over 0s and 1s
+// (2) COMPILING = CONVERT TO MACHINE CODE (happens inside the JS engine)
+
+// 1.4 Multi-paradigm
+// (1) Paradigm: an approach and mindset of structuring code, which will direct your coding style and technique.
+// (2) Three paradigms:
+// a. Procudual programming
+// b. Object-oriented programming (OOP)
+// c. Functional programming (FP)
+// (3) Imperative vs. Declarative
+
+// 1.5 Prototype-based object-oriented
+
+// 1.6 First-class functions
+// Meaning functions in that language are treated like any other variable.
+
+// 1.7 Dynamic (Dynamically-typed language)
+// (1) No data type definitions. Types becomes known at runtime
+// (2) Data type of variable is automatically changed
+
+// 1.8 Single-threaded
+// 1.9 Non-blocking event loop
+// (1) Concurrency model (How the JavaScript engine handles multiple tasks happening at the same time)
+// -- Why do we need this? -->
+// (2) JavaScript runs on one single thread (Thread means where the code is executed in the processor/CPU), which it can only execute one task at a time
+// -- What about a long-running task? -->
+// (3) Sounds like it will block the single thread. But we need NON-blocking behavior
+// -- How can we achieve that? -->
+// (4) By using Event Loop: takes long running tasks, executes them in the 'background', and puts them back in the main thread once they are completed.
 ///////////////////////////////////////////////////
 
 // NOTE 2. The JavaScript Engine and Runtime
