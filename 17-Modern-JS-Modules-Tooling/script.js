2e0689721e9e65cc70755c4b8129733e2fa2da32
Howie Hao Wang <howiewang2020@gmail.com>
2021-06-26 09:00:43 -0400

    Initial Commit: 17-modern-js

---
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -0,0 +1,16 @@
+'use strict';
+// NOTE Index
+// 1 An Overview of Modern JavaScript Development
+// 2 An Overview of Modules in JavaScript
+// 3 Exporting and Importing in ES6 Modules
+// 4 The Module Patter
+// 5 CommonJS Modules
+// 6 A Brief Introduction to the Command Line
+// 7 Introduction to NPM
+// 8 Bundling with Parcel and NPM Scripts
+// 9 Configuring Babel and Polyfilling
+// 10 Review: Writing Clean and Modern JavaScript
+// 11 Let's Fix Some Bad Code: Part 1
+// 12 Declarative and Functional JavaScript Principles
+// 13 Let's Fix Some Bad Code: Part 2
+/////////////////////////////////////////////////////////////
