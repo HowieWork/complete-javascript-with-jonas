86299e4d00ca4055c8ea754cc5d6daa0ed5dc095
Howie Hao Wang <howiewang2020@gmail.com>
2021-07-07 15:37:18 -0400

    Update script.js

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -8,6 +8,7 @@
 // 7 Introduction to NPM
 // 8 Bundling with Parcel and NPM Scripts
 // 9 Configuring Babel and Polyfilling
+
 // 10 Review: Writing Clean and Modern JavaScript
 // 11 Let's Fix Some Bad Code: Part 1
 // 12 Declarative and Functional JavaScript Principles
