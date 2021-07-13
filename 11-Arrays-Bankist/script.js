645a6f1e61b2e4dfda63456308e5060b3bd45204
Howie <howiewang2020@gmail.com>
2021-07-13 18:08:00 -0400

    Update section-11

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -61,6 +61,132 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+  movements.forEach((mov, i) => {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__date"></div>
+        <div class="movements__value">${mov} €</div>
+      </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov);
+  labelBalance.textContent = `${acc.balance} €`;
+};
+
+const calcDisplaySummary = function (acc) {
+  const movements = acc.movements;
+  const interestRate = acc.interestRate;
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes} €`;
+
+  const out = Math.abs(
+    movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov, 0)
+  );
+  labelSumOut.textContent = `${out} €`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(mov => (mov * interestRate) / 100)
+    .filter(int => int >= 1)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumInterest.textContent = `${interest} €`;
+};
+
+const updateUI = function (acc) {
+  // Display movements
+  displayMovements(acc.movements);
+
+  // Display balance
+  calcDisplayBalance(acc);
+
+  // Display summary
+  calcDisplaySummary(acc);
+};
+
+const createUsernames = function (accs) {
+  accs.forEach(
+    acc =>
+      (acc['username'] = acc.owner
+        .toLowerCase()
+        .split(' ')
+        .map(name => name[0])
+        .join(''))
+  );
+};
+createUsernames(accounts);
+
+// Event handler
+let currentAccount;
+btnLogin.addEventListener('click', function (e) {
+  e.preventDefault();
+  const username = inputLoginUsername.value;
+  const pin = inputLoginPin.value;
+  currentAccount = accounts.find(acc => acc.username === username);
+
+  // Clear input fields and move away focus
+  inputLoginUsername.value = '';
+  inputLoginPin.value = '';
+  inputLoginPin.blur();
+
+  if (!currentAccount) {
+    console.error('Account does not exist!');
+    return;
+  }
+  if (+pin !== currentAccount.pin) {
+    console.log('Pin is not correct');
+    return;
+  }
+
+  // Display UI and message
+  labelWelcome.textContent = `Welcome back, ${
+    currentAccount.owner.split(' ')[0]
+  }`;
+  containerApp.style.opacity = 1;
+
+  updateUI(currentAccount);
+});
+
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const transferTo = inputTransferTo.value;
+  const transferAmount = +inputTransferAmount.value;
+  const receiverAcc = accounts.find(acc => acc.username === transferTo);
+
+  // Clear inputs
+  inputTransferTo.value = inputTransferAmount.value = '';
+
+  if (
+    !receiverAcc ||
+    transferAmount <= 0 ||
+    transferAmount > currentAccount.balance ||
+    currentAccount.username === receiverAcc.username
+  ) {
+    console.error('Transfer is not successful.');
+    return;
+  }
+  // 1. current account
+  currentAccount.movements.push(-transferAmount);
+
+  // 2. transfer to account
+  receiverAcc.movements.push(+transferAmount);
+
+  // 3. Update current account movements, balance, summary
+  updateUI(currentAccount);
+});
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -72,5 +198,4 @@ const currencies = new Map([
 ]);
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
 /////////////////////////////////////////////////
