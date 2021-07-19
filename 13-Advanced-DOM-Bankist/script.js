20547f578c9393ce609ccd7b0630adc085bb8831
Howie <howiewang2020@gmail.com>
2021-07-19 10:54:59 -0400

    Update script.js in section 13

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -1,12 +1,16 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+///////////////////////////////////////
+
+// Modal window
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -27,6 +31,80 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) closeModal();
 });
 ///////////////////////////////////////
+
+// Button scrolling
+btnScrollTo.addEventListener('click', function (e) {
+  // Method 1 * old way
+  /*
+  const s1coords = section1.getBoundingClientRect();
+  window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior: 'smooth',
+  });
+  */
+  // Method 2
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+///////////////////////////////////////
+
+// Page navigation
+/*
+document.querySelectorAll('.nav__link').forEach(el => {
+  el.addEventListener('click', e => {
+    e.preventDefault();
+    const id = el.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  });
+});
+*/
+
+// IMPORTANT Pattern
+// 1. Add event listener to common parent element
+// 2. Determine which element orignates the event
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+///////////////////////////////////////
+
+// Tabbed components
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Clear --active classes for btn and content
+  tabs.forEach(tab => {
+    tab.classList.remove('operations__tab--active');
+  });
+  tabsContent.forEach(tabContent => {
+    tabContent.classList.remove('operations__content--active');
+  });
+
+  // Add --active class to btn
+  clicked.classList.toggle('operations__tab--active');
+
+  // Matching target content and add --active class
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.toggle('operations__content--active');
+});
+
+///////////////////////////////////////
+/*
 // NOTE 1. Selecting, Creating/Inserting, Deleting elements
 // 1.1 Selecting elements
 // (1) querySelector
@@ -57,8 +135,9 @@ document.querySelector('.btn--close-cookie').addEventListener('click', () => {
   // message.parentElement.removeChild(message); // old way
   message.remove(); // remove method is new
 });
-
+*/
 ///////////////////////////////////////
+/*
 // NOTE 2. Styles, Attributes and Classes
 // 2.1 Styles
 const btnCloseCookie = message.querySelector('.btn');
@@ -79,6 +158,7 @@ console.log(getComputedStyle(message).height);
 
 // 2.2 Attributes
 // (1) Get attr
+
 const logo = document.querySelector('.nav__logo');
 console.log(logo.src); // -> absolute URL
 console.log(logo.getAttribute('src')); // -> relative URL
@@ -110,3 +190,50 @@ logo.classList.replace('c', 'd');
 
 // IMPORTANT DON'T use below:
 // logo.className = 'jonas'
+*/
+///////////////////////////////////////
+
+// NOTE 3. Types of Events and Event Handlers
+// Method 1 *Old way
+// h1.onmouseenter = function () {
+//   alert('You entered h1!');
+// };
+
+// Method 2
+// Patterns: Event only happens one time
+const h1 = document.querySelector('h1');
+const h1Alert = function () {
+  alert('You entered h1!');
+  h1.removeEventListener('mouseenter', h1Alert);
+};
+// h1.addEventListener('mouseenter', h1Alert);
+
+///////////////////////////////////////
+
+// NOTE 4. DOM Traversing
+/*
+const highlights = h1.querySelectorAll('.highlight');
+// 4.1 Going downwards: child
+console.log(highlights);
+console.log(h1.childNodes);
+console.log(h1.children); // live HTMLCollection
+h1.firstElementChild.style.color = 'white';
+
+// 4.2 Going upwards: parent
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+// Think of closest() as the opposite of querySelector
+h1.closest('header').style.background = 'var(--gradient-secondary)';
+console.log(h1.closest('h1')); // -> <h1>...</h1>
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// 4.3 Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+*/
+///////////////////////////////////////
