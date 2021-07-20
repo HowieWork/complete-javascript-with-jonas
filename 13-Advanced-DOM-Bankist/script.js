84c2000fcabce13b23bc5182967483671ca2dc7e
Howie <howiewang2020@gmail.com>
2021-07-20 11:12:01 -0400

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -198,11 +198,113 @@ const sectionObserver = new IntersectionObserver(revealSection, {
   root: null,
   threshold: 0.15,
 });
-
+// FIXME remove /* */
+/*
 allSections.forEach(section => {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+*/
+///////////////////////////////////////
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+imgTargets.forEach(img => {
+  imgObserver.observe(img);
+});
+///////////////////////////////////////
+
+// Build a slider component
+const slider = function () {
+  const slides = document.querySelectorAll('.slide');
+  const maxSlide = slides.length;
+  const btnLeft = document.querySelector('.slider__btn--left');
+  const btnRight = document.querySelector('.slider__btn--right');
+  const dotsContainer = document.querySelector('.dots');
+
+  let curSlide = 0;
+
+  const createDots = function () {
+    slides.forEach((_, i) => {
+      const btn = document.createElement('button');
+      btn.classList.add('dots__dot');
+      btn.setAttribute('data-slide', i);
+      dotsContainer.appendChild(btn);
+    });
+  };
+  const activateDot = function (slide) {
+    document.querySelectorAll('.dots__dot').forEach(dot => {
+      dot.classList.remove('dots__dot--active');
+      if (+dot.dataset.slide === slide) dot.classList.add('dots__dot--active');
+    });
+  };
+  const goToSlide = function (slide) {
+    slides.forEach((s, i) => {
+      s.style.transform = `translateX(${100 * (i - slide)}%)`;
+    });
+    activateDot(slide);
+  };
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+    goToSlide(curSlide);
+  };
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+  };
+
+  // Initial
+  goToSlide(0);
+  createDots();
+  activateDot(0);
+
+  // Hanlde left/right buttons
+  btnRight.addEventListener('click', nextSlide);
+  btnLeft.addEventListener('click', prevSlide);
+
+  // Hanlde left/right arrow keys
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowLeft') prevSlide();
+    e.key === 'ArrowRight' && nextSlide();
+  });
+
+  // Handle dots
+  dotsContainer.addEventListener('click', function (e) {
+    if (e.target.classList.contains('dots__dot')) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activateDot(+slide);
+    }
+  });
+};
+slider();
+
 ///////////////////////////////////////
 /*
 // NOTE 1. Selecting, Creating/Inserting, Deleting elements
